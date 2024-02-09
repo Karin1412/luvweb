3307d6954fa6b87ae2eafdbf68bd240a3ab6060e
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-02-09 17:53:39 +0700

    update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header =() =>{
     return(
         <div>
-            <h1 className='underline text-3xl font-bold text-center text-pink-900'>
+            <h1 className='underline text-3xl font-bold text-center text-pink-900 mb-10'>
                 Karin web
             </h1>
         </div>
