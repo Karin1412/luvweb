3fe122468c091105a3b5c22df76d199c5d2627d0
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-02-09 14:50:49 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Item from './components/Item'
+import VideoComponent from './components/Video'
 
 export default function App() {
   return (
     <div className='bg-pink-50 p-4'>
       <Header></Header>
       <Item Image='https://i.pinimg.com/236x/2f/12/94/2f12942fc56d1ccf226acb1d4daf92d3.jpg' Name='Mind'></Item>
+      <VideoComponent></VideoComponent>
     </div>
   )
 }
