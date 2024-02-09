55a5223cbe963b1e286f90d52c5e4e4438177c26
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-02-09 20:11:39 +0700

    update folder

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Header from './components/Header'
-import Item from './components/Item'
-import VideoComponent from './components/Video'
+import Item from './components/CoupleComponent/Item'
+import VideoComponent from './components/VideoComponent'
 
 export default function App() {
   return (
-    <div className='bg-pink-50 p-4'>
+    <div className ='bg-pink-50 w-full p-2'>
       <Header></Header>
-      <Item Image='https://i.pinimg.com/236x/2f/12/94/2f12942fc56d1ccf226acb1d4daf92d3.jpg' Name='Mind'></Item>
-      <VideoComponent></VideoComponent>
+      <div className='flex-1 md:flex'>
+        <Item Image='https://i.pinimg.com/236x/2f/12/94/2f12942fc56d1ccf226acb1d4daf92d3.jpg' Name='Mind'></Item>
+        <VideoComponent></VideoComponent>
+      </div>
     </div>
   )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,8 +13,8 @@ const Header = () => {
 
   return (
     <div>
-      <h1 className={`underline text-3xl font-custom font-bold text-center ${textColor ? 'text-red-400' : 'text-pink-700'} mb-10`}>
-        Karin web
+      <h1 className={`underline text-3xl font-custom italic font-bold text-center ${textColor ? 'text-red-400' : 'text-pink-700'} mb-10`}>
+        HAPPY NEW YEAR, MY BAE!!! 
       </h1>
     </div>
   );
