e76b61d17d246b17284929c249485526c62957a1
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-02-09 18:38:19 +0700

    update Item

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,26 +2,27 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
+const Item = (props) => {
+  const [isLiked, setLiked] = useState(false);
 
-const Item = (props) =>{
-    const [isLiked, setLiked] = useState(false)
-    const handleClick =() =>{
-        setLiked(!isLiked)
-    }
-    return(
-        <div className='rounded-lg bg-white p-3 w-56 h-full m-2'>
-            <img src={props.Image} alt='avatar' className='rounded-md'/>
-            <div className='flex'>
-                <p className='text-pink-400 font-semibold font-serif text-lg mt-1 ml-2'>{props.Name}</p>
-                <FontAwesomeIcon
-                    className='h-6 w-6 m-1 ml-28'
-                    icon={faHeart}
-                    style={{color: isLiked ? 'FF5675' : 'E1E1E1' } }
-                    onClick={handleClick}>
-                </FontAwesomeIcon>
-            </div>
-        </div>
-    )
-}
+  const handleClick = () => {
+    setLiked(!isLiked);
+  };
 
-export default Item
+  return (
+    <div className='rounded-lg bg-white p-3 w-56 h-full m-2'>
+      <img src={props.Image} alt='avatar' className='rounded-md' />
+      <div className='flex justify-between items-center'>
+        <p className='text-pink-400 font-semibold font-serif text-lg mt-1 ml-2'>{props.Name}</p>
+        <FontAwesomeIcon
+          className='h-6 w-6 m-1'
+          icon={faHeart}
+          style={{ color: isLiked ? '#FF5675' : '#E1E1E1' }}
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Item;
