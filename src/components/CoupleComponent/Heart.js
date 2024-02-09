fd56fca7f74c0f6f601c88600424a9eb327837dc
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-02-09 21:07:04 +0700

    update Heart

--- src/components/CoupleComponent/Heart.js
+++ src/components/CoupleComponent/Heart.js
@@ -4,14 +4,32 @@ import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 const Heart = () =>{
     const [isBeat, setBeat] = useState(false)
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+          setBeat((prevColor) => !prevColor);
+        }, 300);
+    
+        return () => clearTimeout(timeoutId);
+      }, [isBeat]);
+    
+    const date = () =>{
+        let firstDay = new Date("12/05/2023")
+        let today = new Date()
 
+        let soMiligiay = Math.abs(firstDay - today);
+
+        let soNgay = Math.floor(soMiligiay / (1000 * 60 * 60 * 24));
+        return soNgay;
+    }
     return(
-        <FontAwesomeIcon
-        icon={faHeart}
-        style={{ color: isBeat ? '#FF5675' : '#E1E1E1' }}
-        className='w-12 h-12'> 
-
-    </FontAwesomeIcon>
+        <div className='p-28 text-center items-center'>
+            <FontAwesomeIcon
+                icon={faHeart}
+                style={{ color: isBeat ? '#FF5977' : '#FF365A' }}
+                className='w-32 h-32'> 
+            </FontAwesomeIcon>
+            <p className='font-custom text-pink-800'>Tụi mình đã bên nhau được {date()} ngày rồi nè</p>
+        </div>
     )
 }
 
