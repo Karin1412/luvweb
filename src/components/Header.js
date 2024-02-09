cbf173eb485b5bb18b06b8c772bd30668e9b241b
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-02-09 18:38:04 +0700

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Header =() =>{
-    return(
-        <div>
-            <h1 className='underline text-3xl font-bold text-center text-pink-900 mb-10'>
-                Karin web
-            </h1>
-        </div>
-    )
-}
+const Header = () => {
+  const [textColor, setTextColor] = useState(false);
 
-export default Header
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setTextColor((prevColor) => !prevColor);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, [textColor]);
+
+  return (
+    <div>
+      <h1 className={`underline text-3xl font-custom font-bold text-center ${textColor ? 'text-red-400' : 'text-pink-700'} mb-10`}>
+        Karin web
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
